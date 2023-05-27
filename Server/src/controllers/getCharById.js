4cd7656e1ebd968c49d99ffaf82e29560d06d627
CamCufre <cufrecamila1@gmail.com>
2023-05-26 22:44:15 -0300

    Create getCharById.js

---
+++ Server/src/controllers/getCharById.js
@@ -0,0 +1,33 @@
+const axios = require('axios')
+const URL = 'https://rickandmortyapi.com/api/character/'
+
+const getCharById = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const response = await axios(`${URL}/${id}`);
+    const { status, name, species, origin, image, gender } = response.data;
+
+    if (name) {
+      const character = {
+        id,
+        name,
+        species,
+        origin,
+        image,
+        gender,
+        status
+      };
+      return res.status(200).json(character);
+    }
+    
+    return res.status(404).send('Not Found');
+    
+  } catch (error) {
+    return res.status(500).send(error.message);
+  }
+};
+
+module.exports = {
+  getCharById
+}
